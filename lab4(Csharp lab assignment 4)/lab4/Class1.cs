ced2c4f2a50c23bc72bb8d17627c9a387890e338
Nathnael <nathealterefe@gmail.com>
2022-07-10 14:39:13 +0300

    c-sharp lab assignment 4

---
+++ lab4(Csharp lab assignment 4)/lab4/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    internal class Class1
+    {
+        public string number { get; set; }
+        public string inventory { get; set; }
+        public string objectname { get; set; }
+        public string count { get; set; }
+        public string price { get; set; }
+
+        public void save()
+        {
+            Console.WriteLine("saved");
+        }
+            
+
+
+
+    }
+}
---
+++ lab4(Csharp lab assignment 4)/lab4/Form2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 c = new Class1
+            {
+                number = number.Text,
+                inventory = inventory.Text,
+                objectname = objectname.Text,
+                count = count.Text,
+                price = price.Text,
+            };
+            MessageBox.Show("saved");
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void price_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
